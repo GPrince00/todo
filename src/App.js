ce0fe1fd5dfee9e79b9169147b5be1d78dacda15
GPrince00 <gustavoprince0@gmail.com>
2021-05-24 22:28:00 -0300

    feat(list): creates the to do list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
 function App() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+
+  const AddTask = (newTask) => {
+    let updatedList = list.slice();
+    updatedList.push(newTask);
+    setList(updatedList);
+    setTask("")
+  }
+
   return (
-    <h1>To do</h1>
+    <Wrapper>
+      <Title>To do</Title>
+      <AddNewTask>
+        <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+        <button onClick={() => AddTask(task)} >Add</button>
+      </AddNewTask>
+      <ListOfTasks>
+        {list.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </ListOfTasks>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #d0d0d0;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-family: 'Courier New', Courier, monospace;
+`;
+
+const AddNewTask = styled.div`
+`;
+
+const ListOfTasks = styled.div`
+`;
